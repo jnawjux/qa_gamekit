3815372e2870ce6368af5c1babfeba343a20c4c2
John Naujoks <johnnaujoks@gmail.com>
2019-11-25 21:14:21 -0800

    Update to work with pickled model

--- api.py
+++ api.py
@@ -2,25 +2,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-import os
-from ast import literal_eval
 import pandas as pd
+import pickle
 
-from cdqa.utils.filters import filter_paragraphs
 from cdqa.pipeline import QAPipeline
+
+cdqa_pipeline = pickle.load(open('gamekit_model.sav', 'rb'))
 
 app = Flask(__name__)
 CORS(app)
-
-dataset_path = os.environ["dataset_path"]
-reader_path = os.environ["reader_path"]
-
-df = pd.read_csv(dataset_path, converters={"paragraphs": literal_eval})
-df = filter_paragraphs(df)
-
-cdqa_pipeline = QAPipeline(reader=reader_path)
-cdqa_pipeline.fit_retriever(df=df)
-
 
 @app.route("/api", methods=["GET"])
 def api():
